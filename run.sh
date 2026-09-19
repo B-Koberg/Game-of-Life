044791ba797a5a3b7eeda4232ceb9f0f7eb94537
B-Koberg <b.koberg412@gmail.com>
2026-09-19 12:59:29 +0200

    analysis of scaling

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ START_MS=$(($(date +%s%N) / 1000000))
 
 echo "=== Build gestartet: $(date '+%Y-%m-%d %H:%M:%S') ==="
 
-fpm build -V
+#fpm build -V
 
 fpm run 
 
@@ -36,7 +36,7 @@ printf "=== Berechnung fertig: %s, Dauer: %02d:%02d:%02d.%03d ===\n" \
     "$(date '+%Y-%m-%d %H:%M:%S')" \
     "$hours" "$mins" "$secs" "$msecs"
 
-python3 make_video.py
+#python3 make_video.py
 
 # Gesamtzeit
 END_MS=$(($(date +%s%N)/1000000))
@@ -51,7 +51,7 @@ printf "=== Komplett fertig: %s, Dauer: %02d:%02d:%02d.%03d ===\n" \
     "$(date '+%Y-%m-%d %H:%M:%S')" \
     "$hours" "$mins" "$secs" "$msecs"
 
-xdg-open output/frames.mp4
+#xdg-open output/frames.mp4
 
 # Symlink auf die aktuelle Logdatei
 ln -sf "$(realpath "$LOGPATH")" "${LOGDIR}/build.log"
